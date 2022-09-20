a6a4f8385e285ea0c65492d22058ca2aa2584c07
annisawm <annisawm200601@gmail.com>
2022-09-20 15:39:56 +0700

    Login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,71 +5,15 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Redirect;
-=======
 use Illuminate\Support\Facades\Session;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
->>>>>>> e29206c70c2c5a8ead2c729c6691dd99fa7b2bb5
 
 class AuthController extends Controller
 {
     public function index()
     {
-<<<<<<< HEAD
-        if ($user = Auth::user()) {
-            if ($user->level == 'super_admin') {
-                return redirect()->intended('super_admin');
-            } elseif ($user->level == 'admin') {
-                return redirect()->intended('admin');
-            } elseif ($user->level == 'operator') {
-                return redirect()->intended('operator');
-            } elseif ($user->level == 'pejabat_pj') {
-                return redirect()->intended('pejabat_pj');
-            } elseif ($user->level == 'pejabat') {
-                return redirect()->intended('pejabat');
-            }
-            return view('login');
-        }
-    }
-
-    public function proses_login(Request $request)
-    {
-        request()->validate(
-            [
-                'username' => 'required',
-                'password' => 'required',
-            ]
-        );
-
-        $kredensil = $request->only('username', 'password');
-
-        if (Auth::attempt($kredensil)) {
-            $user = Auth::user();
-            if ($user->level == 'super_admin') {
-                return redirect()->intended('super_admin');
-            } elseif ($user->level == 'admin') {
-                return redirect()->intended('admin');
-            } elseif ($user->level == 'operator') {
-                return redirect()->intended('operator');
-            } elseif ($user->level == 'pejabat_pj') {
-                return redirect()->intended('pejabat_pj');
-            } elseif ($user->level == 'pejabat') {
-                return redirect()->intended('pejabat');
-            }
-            return redirect()->intended('/');
-        }
-
-        return redirect('login')
-            ->withInput()
-            ->withErrors(['login_gagal' => 'Kredensial ini tidak cocok dengan data mana pun']);
-    }
-
-    public function logout(Request $request)
-    {
-        $request->session()->flush();
-=======
         return view('auth.login');
     }
 
@@ -86,7 +30,7 @@ class AuthController extends Controller
         ]);
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
-            return redirect()->intended('guest')
+            return redirect()->intended('home')
                 ->withSuccess('You have Successfully loggedin');
         }
         return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
@@ -100,7 +44,7 @@ class AuthController extends Controller
         ]);
         $data = $request->all();
         $check = $this->create($data);
-        return redirect("guest")->withSuccess('Great! You have Successfully loggedin');
+        return redirect("home")->withSuccess('Great! You have Successfully loggedin');
     }
 
     public function create(array $data)
@@ -115,7 +59,6 @@ class AuthController extends Controller
     public function logout()
     {
         Session::flush();
->>>>>>> e29206c70c2c5a8ead2c729c6691dd99fa7b2bb5
         Auth::logout();
         return Redirect('login');
     }
